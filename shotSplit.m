32f4913ade5ce350409042b754e872a935dcb747
Evgeny Toropov <etoropov@andrew.cmu.edu>
2013-11-06 15:34:31 -0500

    adds some pictures for the report

--- shotSplit.m
+++ shotSplit.m
@@ -1,14 +1,17 @@
-filename = 'videoFragments/Liverpool_Man_Utd_1_September_2013-1st-10-sm.mp4';
+filename = 'videoData/videoFragments/Liverpool_Man_Utd_1_September_2013-1st-10-sm.mp4';
+
+outTemplate = 'videoData/videoShots/Liverpool_Man_Utd_1_September_2013-1st-10/';
 
 %obj = VideoReader(filename);
 %video = read(obj);
 
 numFrames = size(video, 4);
-numBinsChannel = 100;
+numBinsChannel = 25;
 
+I2 = video(:,:,:,1);
 for iFrame = 1 : numFrames - 1
 
-    I1 = video(:,:,:,iFrame);
+    I1 = I2;
     I2 = video(:,:,:,iFrame+1);
 
     h1red   = imhist(I1(:,:,1), numBinsChannel);
@@ -22,6 +25,32 @@ for iFrame = 1 : numFrames - 1
     h2 = [h2red; h2green; h2blue];
 
     diff(iFrame) = sum(abs(h1 - h2)) / 2 / size(I1,1) / size(I1,2);
-    diff(iFrame)
+    if (mod(iFrame, 100) == 0)
+        fprintf('frame %d \n', iFrame);
+    end
     
 end
+
+
+for iFrame = 1 : 
+    
+    
+% write data to file
+for i
+    outFilename = fullfile (outTemplate, int2str(iFrame)+'.avi');
+    writerObj = VideoWriter (filename, 'MPEG-4');
+    writeVideo(writerObj,images);
+
+
+% gaussSigma = 100; % frames
+% 
+% 
+% size = gaussSigma * 2 + 1;
+% x = linspace(-size / 2, size / 2, size);
+% gaussFilter = exp(-x .^ 2 / (2 * gaussSigma ^ 2));
+% gaussFilter = gaussFilter / sum (gaussFilter); % normalize
+% 
+% yfilt = conv (diff, gaussFilter, 'same');
+% plot (diff ./ yfilt);
+
+
